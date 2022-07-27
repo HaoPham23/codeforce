5c7f1c5dfdf0b6ef44ba571cc8bca23c5969d003
Pham Duc Hao <hao.pham1652003@gmail.com>
2022-07-27 18:53:22 +0700

    HQ9+

---
+++ HQ9+.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+    for(int i = 0; s[i]; i++) {
+        if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9') {
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+    return 0;
+}
