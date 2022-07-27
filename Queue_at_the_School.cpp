3c7fa5319855e04f4c08bcbe791869148fa26c7f
Pham Duc Hao <hao.pham1652003@gmail.com>
2022-07-27 08:13:08 +0700

    Queue at the School

--- Queue_at_the_School.cpp
+++ Queue_at_the_School.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string solve(string s) {
+    for(int i = 1; s[i]; i++) {
+        if(s[i] == 'G' && s[i-1] == 'B') {
+            swap(s[i], s[i-1]);
+            i++;
+        }
+    }
+    return s;
+}
+
 int main() {
     int n, t;
     cin >> n >> t;
     string s;
     cin >> s;
-    
+    for(int i = 0; i < t; i++) {
+        s = solve(s);
+    }
+    cout << s;
     return 0;
 }
